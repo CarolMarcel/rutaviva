a62303be3b472a4cb0efd365ebb609dc8b1723ac
Carol Marcel <cg.marcel@gmail.com>
2025-10-02 16:07:50 -0300

    modificacion

--- reservas-frontend/src/main.jsx
+++ reservas-frontend/src/main.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
-import {ClientDashboard} from ".reservas-frontend/src/pages/admin/AdminDashboard.jsx";
-import AdminDashboard from ".reservas-frontend/src/pages/client/ClientDashboard.jsx";
-import StaffDashboard from ".reservas-frontend/src/pages/staff/StaffDashboard.jsx";
+import {ClientDashboard} from "./src/pages/admin/AdminDashboard.jsx";
+import AdminDashboard from "./src/pages/client/ClientDashboard.jsx";
+import StaffDashboard from "./src/pages/staff/StaffDashboard.jsx";
 
+// Guardia por rol
 const Private = ({ children, roles }) => {
   const token = localStorage.getItem("rv_token");
   const role = localStorage.getItem("rv_role");
+
   if (!token) return <Navigate to="/login" replace />;
   if (roles && !roles.includes(role)) return <Navigate to="/login" replace />;
   return children;
@@ -17,9 +19,36 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Private roles={['CLIENT','ADMIN','STAFF']}><ClientDashboard/></Private>} />
-        <Route path="/admin" element={<Private roles={['ADMIN']}><AdminDashboard/></Private>} />
-        <Route path="/staff" element={<Private roles={['ADMIN','STAFF']}><StaffDashboard/></Private>} />
+
+        {/* Cliente */}
+        <Route
+          path="/"
+          element={
+            <Private roles={["CLIENT", "ADMIN", "STAFF"]}>
+              <ClientDashboard />
+            </Private>
+          }
+        />
+
+        {/* Admin */}
+        <Route
+          path="/admin"
+          element={
+            <Private roles={["ADMIN"]}>
+              <AdminDashboard />
+            </Private>
+          }
+        />
+
+        {/* Colaborador */}
+        <Route
+          path="/staff"
+          element={
+            <Private roles={["ADMIN", "STAFF"]}>
+              <StaffDashboard />
+            </Private>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
